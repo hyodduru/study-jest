49925f0eee013b9c2517b2825e2603990b0bb837
hyodduru <hoje15v@gmail.com>
2022-09-27 19:22:17 +0900

    버튼 색상 클릭 테스트

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div />;
+  const [buttonColor, setButtonColor] = useState("red");
+  const newButtonColor = buttonColor === "red" ? "blue" : "red";
+
+  return (
+    <div>
+      <button
+        onClick={() => setButtonColor("blue")}
+        style={{ backgroundColor: buttonColor }}
+      >
+        Change to {newButtonColor}
+      </button>
+    </div>
+  );
 }
 
 export default App;
