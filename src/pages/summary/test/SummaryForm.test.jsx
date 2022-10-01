fdbc2e8977d4868d6db1027a0cecf90908682a50
hyodduru <hoje15v@gmail.com>
2022-10-01 18:37:28 +0900

    팝오버 비동기 테스트코드

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
 import userEvent from "@testing-library/user-event";
 
@@ -28,8 +32,25 @@ test("Checkbox disables button on first click and enables on second click", () =
   expect(confirmButton).toBeDisabled();
 });
 
-test("popover responds to hover", () => {
+test("popover responds to hover", async () => {
+  render(<SummaryForm />);
   // popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actully be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
   // popover appears upon mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  userEvent.hover(termsAndConditions);
+
+  const popover = screen.getByText(/no ice cream will actully be delivered/i);
+  expect(popover).toBeInTheDocument();
   // popover disappears when we mouse out
+  userEvent.unhover(termsAndConditions);
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream will actully be delivered/i)
+  );
 });
+
+// 팝오버가 사라지는 동작 비동기적으로 일어나고 있음. 즉 테스트 함수가 완료되고 나서야 동작이 일어난 것. => 단언을 비동기화함으로써 문제 해결 가능.
